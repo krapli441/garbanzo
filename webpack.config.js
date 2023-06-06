3720a8ca7bfa4d96624ae925cf3f837be6afdd94
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-06 13:33:55 +0900

    기초 파일 생성

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.tsx",
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        use: ["babel-loader"],
+      },
+      {
+        test: [/\.ts$/, /\.tsx$/],
+        use: ["ts-loader"],
+      },
+      {
+        test: [/\.css$/],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: [/\.png$/],
+        use: ["file-loader"],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 8085,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".css", ".png"],
+  },
+};
