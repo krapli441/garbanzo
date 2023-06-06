52d3e13eeaaf5393158953a85b75915557ed26bc
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-06 13:46:56 +0900

    리액트 세팅 완료. 번들 파일 생성 확인. 이후 작업 브랜치 생성하여 작업할 것

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,13 @@
 import React from "react";
+import { render } from "react-dom";
+import { createRoot, Root } from "react-dom/client";
+
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 import React from "react";
+import { render } from "react-dom";
+import { createRoot, Root } from "react-dom/client";
+import App from "./App";
 
-
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
