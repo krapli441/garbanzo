b20d05ba911cdad3854801650b6d84da359508b0
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-17 16:09:43 +0900

    텍스트 크기가 무작위로 변하지만 컴포넌트를 렌더링 할 때마다 styled.component를 실행함.

--- src/components/views/main.tsx
+++ src/components/views/main.tsx
@@ -1,5 +1,5 @@
 // 리액트 라이브러리
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 // 리액트 컴포넌트
@@ -18,34 +18,33 @@ const MainStyle = styled.div`
   color: white;
 `;
 
-// main 텍스트 스타일링
+// Pretendard 폰트 속성을 가져옴.
 const MyName = styled.p`
   font-size: 48px;
-  font-weight: bold;
+  font-weight: ;
 `;
 
-// Pretendard 폰트 속성을 가져옴.
-const fontWeight: (string | number)[] = [
-  100,
-  200,
-  300,
-  400,
-  500,
-  600,
-  700,
-  800,
-  900,
-  "bold",
-  "bolder",
-  "lighter",
-  "normal",
-];
-
 export default function Main() {
+  const [fontWeight, setFontWeight] = useState("bold");
+  const fontType: Array<string | number> = [
+    100, 200, 300, 400, 500, 600, 700, 800, 900,
+  ];
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const randomFontWeight = Math.floor(Math.random() * fontType.length);
+      const newFontWeight = String(fontType[randomFontWeight]);
+      setFontWeight(newFontWeight);
+    }, 1000);
+  }, []);
+  // main 텍스트 스타일링
+
   return (
     <>
       <MainStyle className="main">
-        <MyName className="nameText">박준형</MyName>
+        <MyName className="nameText">
+          <span>박준형</span>
+        </MyName>
       </MainStyle>
     </>
   );
