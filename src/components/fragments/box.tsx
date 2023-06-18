bec6a70fca216905d37effdb8f72f0f9048bd94e
krapli441 <tlfqj1208@gmail.com>
2023-06-18 13:56:53 +0900

    box 컴포넌트를 가져올 수 없는 현상

--- src/components/fragments/box.tsx
+++ src/components/fragments/box.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, ReactNode } from "react";
 import styled from "styled-components";
 
 const Box = styled.div`
@@ -12,12 +12,34 @@ const Box = styled.div`
   padding: 10px 20px;
   border: 2px solid white;
   transition: 0.4s ease;
-
+  cursor: pointer;
   &:hover,
   &:focus {
-    width: 230px;
-    height: 230px;
+    width: 220px;
+    height: 220px;
   }
 `;
 
-export default Box;
+type BoxProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+
+export default function ClickBox() {
+  const [expand, setExpand] = useState(false);
+
+  const handleClick = () => {
+    setExpand((prevExtend) => !prevExtend);
+  };
+
+  return (
+    <Box
+      style={{
+        width: expand ? "220px" : "200px",
+        height: expand ? "220px" : "200px",
+      }}
+      onClick={handleClick}
+    ></Box>
+  );
+}
