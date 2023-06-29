a6476a6f4ad10e3f19754a2fb8947f39cc837587
krapli441 <tlfqj1208@gmail.com>
2023-06-29 22:40:05 +0900

    소개 부분에 대한 하이라이트 마크 추가

--- src/components/views/introduce.tsx
+++ src/components/views/introduce.tsx
@@ -1,6 +1,6 @@
 // 리액트 라이브러리
 import React from "react";
-import { Box, Text, Image } from "@chakra-ui/react";
+import { Box, Text, Image, Highlight, Heading } from "@chakra-ui/react";
 import YouTube from "react-youtube";
 
 // 리액트 컴포넌트
@@ -15,18 +15,69 @@ const Introduce: React.FC = () => {
       height={"100%"}
       display={"flex"}
     >
-      <Box className="fisrt" width={"50%"} height={"100%"}>
-        <Text>
-          좋은 디자인이 곧 좋은 경험으로 이어진다고 믿습니다.<br></br>
-          UI, UX에 깊은 관심을 갖고 있습니다.<br></br>
-          인간에 대한 탐구를 즐겨 합니다.<br></br>
-          세상의 좋은 모든 음악들을 사랑합니다.<br></br>
-          여유가 있을 때 음악을 틀며 스트레스를 풉니다.<br></br>
-        </Text>
+      <Box
+        className="fisrt"
+        width={"50%"}
+        height={"100%"}
+        display={"flex"}
+        flexDirection={"column"}
+        gap={"20px"}
+        padding={"20px"}
+      >
+        <Heading>
+          <Highlight
+            query={["좋은 디자인", "좋은 경험"]}
+            styles={{ px: "1", py: "1", bg: "white" }}
+          >
+            좋은 디자인이 곧 좋은 경험으로 이어진다고 믿습니다.
+          </Highlight>
+        </Heading>
+        <Heading size={"2xl"}>
+          <Highlight
+            query={["UI", "UX"]}
+            styles={{ px: "1", py: "1", bg: "white" }}
+          >
+            UI, UX에 깊은 관심을 갖고 있습니다.
+          </Highlight>
+        </Heading>
+        <Heading>
+          <Highlight
+            query={["탐구"]}
+            styles={{ px: "1", py: "1", bg: "white" }}
+          >
+            인간에 대한 탐구를 즐겨 합니다.
+          </Highlight>
+        </Heading>
+        <Heading size={"2xl"}>
+          <Highlight
+            query={["음악", "사랑"]}
+            styles={{ px: "1", py: "1", bg: "white" }}
+          >
+            세상의 모든 음악들을 사랑합니다.
+          </Highlight>
+        </Heading>
+
+        <Heading>
+          <Highlight
+            query={["디제잉"]}
+            styles={{ px: "1", py: "1", bg: "white" }}
+          >
+            여유가 있을 때 디제잉을 하며 스트레스를 풉니다.
+          </Highlight>
+        </Heading>
       </Box>
       <Box className="second" width={"50%"} height={"100%"}>
         <VideoPlayer />
-        <Box className="imageContainer">
+        <Box
+          className="imageContainer"
+          width={"100%"}
+          height={"50%"}
+          display={"flex"}
+          flexWrap={"wrap"}
+        >
+          <Image src={MyProfile} alt="본인" boxSize={"50%"} />
+          <Image src={MyProfile} alt="본인" boxSize={"50%"} />
+          <Image src={MyProfile} alt="본인" boxSize={"50%"} />
           <Image src={MyProfile} alt="본인" boxSize={"50%"} />
         </Box>
       </Box>
