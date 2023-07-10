f0d6923c279edc01ba5f2f2e5fc5d671617be64b
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-10 20:17:21 +0900

    배포 전 커밋

--- src/index.tsx
+++ src/index.tsx
@@ -14,17 +14,17 @@ import { getAnalytics } from "firebase/analytics";
 // ChakraUI
 import { ChakraProvider } from "@chakra-ui/react";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD9bRdOmVS1arflDwbTJgM1ji0-Vt_JeHE",
-//   authDomain: "krapli441portfolio.firebaseapp.com",
-//   projectId: "krapli441portfolio",
-//   storageBucket: "krapli441portfolio.appspot.com",
-//   messagingSenderId: "154267580809",
-//   appId: "1:154267580809:web:a00aa7160578f8aae48958",
-//   measurementId: "G-JTBCG8GVM1",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyD9bRdOmVS1arflDwbTJgM1ji0-Vt_JeHE",
+  authDomain: "krapli441portfolio.firebaseapp.com",
+  projectId: "krapli441portfolio",
+  storageBucket: "krapli441portfolio.appspot.com",
+  messagingSenderId: "154267580809",
+  appId: "1:154267580809:web:a00aa7160578f8aae48958",
+  measurementId: "G-JTBCG8GVM1",
+};
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
